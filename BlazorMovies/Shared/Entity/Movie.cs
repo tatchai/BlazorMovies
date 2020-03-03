93454b172b970580af19fefa0898e8e6783a8d34
Tatchai Piyakulchaidej <tp@uccbox.com>
2020-03-03 15:45:50 +0700

    start

---
+++ BlazorMovies/Shared/Entity/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Entity
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
